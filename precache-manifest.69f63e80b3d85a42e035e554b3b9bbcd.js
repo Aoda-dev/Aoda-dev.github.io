c852532e6b5ad87618c4f871b717762b74117e21
Aoda-dev <66174034+Aoda-dev@users.noreply.github.com>
2020-09-26 16:21:14 +0600

    Add files via upload

---
+++ precache-manifest.69f63e80b3d85a42e035e554b3b9bbcd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0010ed72b6a9f3673274c7e0c1156ff6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37a327cde83350524d4b",
+    "url": "./static/css/main.632da47d.chunk.css"
+  },
+  {
+    "revision": "5cae361313d3201ed0f7",
+    "url": "./static/js/2.0577489f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0577489f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a327cde83350524d4b",
+    "url": "./static/js/main.7f71772d.chunk.js"
+  },
+  {
+    "revision": "5acfd12ba8d6c9ddfbe3",
+    "url": "./static/js/runtime-main.94c40845.js"
+  },
+  {
+    "revision": "11107ec866d732dfc111591abe085f6d",
+    "url": "./static/media/back-circle.11107ec8.png"
+  },
+  {
+    "revision": "7b2f7e1a50ccadb42334aa0fb370478a",
+    "url": "./static/media/Прямоугольник 24.7b2f7e1a.png"
+  },
+  {
+    "revision": "45b58686c379545751a00ca2ed301bba",
+    "url": "./static/media/Прямоугольник 25.45b58686.png"
+  },
+  {
+    "revision": "d69b9367c7997e4663281cd65f5bec0d",
+    "url": "./static/media/Прямоугольник 26.d69b9367.png"
+  },
+  {
+    "revision": "b83d3ca6858ffc4d3caef8bdb717dfb4",
+    "url": "./static/media/Прямоугольник 27.b83d3ca6.png"
+  },
+  {
+    "revision": "23601cf5c7028ba4b18e0c2d62479c25",
+    "url": "./static/media/Прямоугольник 28.23601cf5.png"
+  },
+  {
+    "revision": "21a10d04058d5de892fa438fe720ca24",
+    "url": "./static/media/—Pngtree—paris eiffel tower black and_4677023.21a10d04.png"
+  }
+]);
